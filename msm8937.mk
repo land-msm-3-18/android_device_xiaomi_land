27f6b06aa91a09e462f00ba80c7c62daa1dc0c8c
sweetyicecare <gabrielseedev@outlook.com>
2020-02-07 11:02:32 +0000

    msm8937-common: Build qualcomm powerHAL

--- msm8937.mk
+++ msm8937.mk
@@ -316,7 +316,10 @@ PRODUCT_COPY_FILES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power@1.2-service-qti
+    power.qcom \
+    android.hardware.power@1.0-impl \
+    android.hardware.power@1.0-service
+
 
 PRODUCT_COPY_FILES += \
     $(VENDOR_PATH)/configs/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml
