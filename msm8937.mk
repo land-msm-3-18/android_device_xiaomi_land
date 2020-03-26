a0370b36b3e6b459c692dd6667adf900c4dddcfa
Ali Hasan <ahb7671@gmail.com>
2020-03-26 12:34:37 +0500

    mido: import data-ipa-cfg-mgr
    https://github.com/LineageOS/android_vendor_qcom_opensource_data-ipa-cfg-mgr
    
    Signed-off-by: Ali Hasan <ahb7671@gmail.com>
    Change-Id: Id3c6ecd576cd7a8a158b646502dee9bbbe30cd23

--- msm8937.mk
+++ msm8937.mk
@@ -45,9 +45,9 @@ PRODUCT_COPY_FILES += \
 
 # ANT+
 PRODUCT_PACKAGES += \
-    AntHalService \
-    com.dsi.ant.antradio_library \
-    libantradio
+#    AntHalService \
+#   com.dsi.ant.antradio_library \
+#   libantradio
 
 # APEX
 PRODUCT_COPY_FILES += \
@@ -280,7 +280,6 @@ PRODUCT_PACKAGES += \
 
 # Permissions
 PRODUCT_COPY_FILES += \
-    external/ant-wireless/antradio-library/com.dsi.ant.antradio_library.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/com.dsi.ant.antradio_library.xml \
     frameworks/native/data/etc/android.hardware.audio.low_latency.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.audio.low_latency.xml \
     frameworks/native/data/etc/android.hardware.bluetooth.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.bluetooth.xml \
     frameworks/native/data/etc/android.hardware.bluetooth_le.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.bluetooth_le.xml \
@@ -451,3 +450,7 @@ PRODUCT_PACKAGES += \
     XiaomiParts \
     XiaomiAddon
 
+# IPA Manager
+PRODUCT_PACKAGES += \
+    ipacm \
+    IPACM_cfg.xml
