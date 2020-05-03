c866c0846812b29f80c6fa0cc7de2c7cf11311ee
sairam1411 <github60>
2020-05-03 14:23:00 +0000

    Revert "msm8937-common: thermal: add device specific thermal hw module"
    This reverts commit a66f34b51d99a001fcd761cf2845147aa9935c44.

--- msm8937.mk
+++ msm8937.mk
@@ -369,8 +369,7 @@ PRODUCT_BOOT_JARS += \
 # Thermal
 PRODUCT_PACKAGES += \
     android.hardware.thermal@1.0-impl \
-    android.hardware.thermal@1.0-service \
-    thermal.msm8937
+    android.hardware.thermal@1.0-service
 
 PRODUCT_COPY_FILES += \
     $(VENDOR_PATH)/configs/thermal-engine.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine.conf
