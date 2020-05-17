FraEgg/android_device_xiaomi_msm8937-common_ra

msm8937-common: Import and bind mount loader config for media swcodec.
 * This config needs to be modified for legacy devices. As
   there is no easy way to split it, import it into device
   tree and bind mount it instead.
 * Head at android-10.0.0_r14.

msm8937.mk
@@ -107,6 +107,10 @@ PRODUCT_PACKAGES += \
     audio.bluetooth.default \
     android.hardware.bluetooth.audio@2.0-impl
 
+# APEX
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/ld.config.txt:$(TARGET_COPY_OUT_SYSTEM)/etc/swcodec/ld.config.txt
+
 # Audio mixer
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/audio_tuning_mixer.txt:$(TARGET_COPY_OUT_VENDOR)/etc/audio_tuning_mixer.txt \

land-msm-3-18/android_device_xiaomi_land

msm8937.mk
@@ -49,6 +49,10 @@ PRODUCT_PACKAGES += \
     com.dsi.ant.antradio_library \
     libantradio
 
+# APEX
+PRODUCT_COPY_FILES += \
+    $(VENDOR_PATH)/configs/ld.config.txt:$(TARGET_COPY_OUT_SYSTEM)/etc/swcodec/ld.config.txt
+
 # Audio
 PRODUCT_PACKAGES += \
     audio.a2dp.default \
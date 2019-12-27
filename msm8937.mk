8211a2fc8409f8de4c16857ade8834c86af1838d
aviraxp <aviraxp@users.noreply.github.com>
2019-12-27 06:39:47 +0530

    msm8937-common: Import and bind mount loader config for media swcodec.
     * This config needs to be modified for legacy devices. As
       there is no easy way to split it, import it into device
       tree and bind mount it instead.
     * Head at android-10.0.0_r14.

--- msm8937.mk
+++ msm8937.mk
@@ -49,6 +49,10 @@ PRODUCT_PACKAGES += \
     com.dsi.ant.antradio_library \
     libantradio
 
+# APEX
+PRODUCT_COPY_FILES += \
+    $(VENDOR_PATH)/configs/ld.config.txt:$(TARGET_COPY_OUT_SYSTEM)/etc/swcodec/ld.config.txt
+
 # Audio
 PRODUCT_PACKAGES += \
     audio.a2dp.default \
