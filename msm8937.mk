f1b562c92a7929bf3f2e50d2113924cc9372f356
Nidhun Balaji T R <nidhunbalaji@gmail.com>
2020-05-20 22:22:54 +0530

    Revert "msm8937-common: Import custom Power HAL"
    This reverts commit cff59a9ead6f4dbb5546b22481bf5c5835ea0002.

--- msm8937.mk
+++ msm8937.mk
@@ -316,7 +316,7 @@ PRODUCT_COPY_FILES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power@1.1-service.custom
+    android.hardware.power@1.2-service-qti
 
 PRODUCT_COPY_FILES += \
     $(VENDOR_PATH)/configs/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml
